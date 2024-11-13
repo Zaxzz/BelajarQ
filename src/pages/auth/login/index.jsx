bd17a26ff3a132abce3a13989c9d5a94b2f51956
synx <ahmadruddin17@gmail.com>
2024-11-13 10:05:08 +0700

    setup folder structure

---
+++ src/pages/auth/login/index.jsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-extrabold text-red-500">haloooooooooooo</h1>
+    </div>
+  );
+}
