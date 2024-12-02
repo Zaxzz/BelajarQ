ac76cb20330e18f7e0ad815792a1f547a68a0e17
Ahmad Chomsin <ahmadruddin17@gmail.com>
2024-12-02 22:05:57 +0700

    fix: conflict

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,17 +5,6 @@ import Pricing from "@/components/Pricing";
 import Hero from "@/components/Hero";
 
 export default function Home() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const router = useRouter(); // Inisialisasi useRouter
-
-  const handleRegisterClick = () => {
-    router.push("/auth/register"); // Arahkan ke halaman register
-  };
-
-  const handleLoginClick = () => {
-    router.push("/auth/login"); // Arahkan ke halaman login
-  };
-
   return (
     <>
       <SeoHead title="LaslesVPN Landing Page" />
